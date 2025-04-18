5c3dea880efda4d7a3f3a7d090c2b09d3441269c
patriccosta <patriccosta0@gmail.com>
2025-04-18 16:51:54 -0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MaraPetsApp from './MaraPetsApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MaraPetsApp />
+  </React.StrictMode>
+)
