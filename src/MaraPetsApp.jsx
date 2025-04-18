93ab543855ddaa45e136815e22c9c448823b7b37
patriccosta <patriccosta0@gmail.com>
2025-04-18 16:53:23 -0300

    Delete src directory

--- src/MaraPetsApp.jsx
+++
@@ -1,6 +0,0 @@
-import { useState } from "react";
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-
-// ... (todo o conteúdo do Canvas foi inserido aqui corretamente)
