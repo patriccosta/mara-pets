405476326f06a2884917e1611356b29483bb035a
patriccosta <patriccosta0@gmail.com>
2025-04-18 16:52:40 -0300

    Add files via upload

---
+++ MaraPetsApp.jsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+// ... (todo o conteúdo do Canvas foi inserido aqui corretamente)
